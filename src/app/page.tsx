6a8b282cd2cbfacffe20d9f308daa468c1eadfd7
TausifAli2000 <tausifali2000@gmail.com>
2025-05-16 10:23:04 +0530

    Commmet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
+import ModeToggle from "@/components/ModeToggle";
+import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
-   <div>
-    Hello Home page
-   </div>
+    <div>
+      <SignedOut>
+        <SignInButton>
+          <Button>
+            Sign In
+          </Button>
+        </SignInButton>
+        <SignUpButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+      <ModeToggle />
+    </div>
   );
 }
